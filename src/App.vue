1cabedd2501cf601423a4738727c71985c22fb91
Maria Lamardo <marialamardo@outlook.com>
2019-06-25 22:56:45 -0400

    Supplement 2: Add animations

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
         <div class="dogs-overlay">
           <h1 class="display-2 text-xs-center">Choose your favorite dogs</h1>
           <v-card class="dog-card">
-            <v-img height="400px" :src="currentDogLink"></v-img>
-            <v-card-actions>
+<transition name="fade">
+  <v-img v-if="currentDogLink" height="400px"
+  :src="currentDogLink"></v-img>
+</transition>            <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn icon @click="addToFavorites" :disabled="isAlreadyInFavorites">
                 <v-icon>favorite</v-icon>
@@ -18,11 +20,16 @@
           </v-card>
 
           <v-container grid-list-md fluid>
-            <v-layout wrap>
-              <v-flex xs6 sm4 md2 v-for="(pet, index) in favoriteDogs" :key="pet">
-                <app-dog :dog="pet" @remove="removeFromFavorites(index)"></app-dog>
-              </v-flex>
-            </v-layout>
+            <transition-group
+  name="slide"
+  tag="v-layout"
+  class="wrap">
+  <v-flex xs6 sm4 md2
+    v-for="(pet, index) in favoriteDogs"
+    :key="pet">
+    <app-dog :dog="pet" @remove="removeFromFavorites(index)"></app-dog>
+  </v-flex>
+</transition-group>
           </v-container>
         </div>
       </v-container>
@@ -51,15 +58,11 @@ export default {
   },
   methods: {
     loadNewDog() {
-      axios
-        .get("https://dog.ceo/api/breeds/image/random")
-        .then(response => {
-          this.currentDogLink = response.data.message;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
+  this.currentDogLink = "";
+  axios.get("https://dog.ceo/api/breeds/image/random").then(response => {
+    this.currentDogLink = response.data.message;
+  });
+},
     addToFavorites() {
       this.favoriteDogs.push(this.currentDogLink);
     },
@@ -106,5 +109,31 @@ h1 {
 .dog-card {
   width: 100%;
   max-width: 600px;
+}
+
+/* Animations */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-enter,
+.slide-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+.slide-leave-active {
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 0.5s;
 }
 </style>

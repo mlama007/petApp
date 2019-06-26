4033e291e77a2ab8ae0ce3335cbe35643dfdcaef
Maria Lamardo <marialamardo@outlook.com>
2019-06-25 22:13:06 -0400

    Finished Workshop

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')

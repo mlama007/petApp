c7d55bb120f7f7814ca21eb3b8c9291a3219ad5a
Mitch Amiano <mcamiano@ncsu.edu>
2019-06-28 17:36:58 -0400

    App root instance listens for app-level notices and queues them up

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ import store from './store'
 import './registerServiceWorker'
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import { mapActions } from 'vuex';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
+  methods: {
+    ...mapActions(['queueNotice'])
+  },
+  beforeMount() {
+    this.$on('app-error', (message) => { this.queueNotice({type: 'error', content: message});});
+    this.$on('app-info', (message) => { this.queueNotice({type: 'info', content: message});});
+  }
 }).$mount('#app')
